9abd7149b378710706c56d3ede115c1bcadeaf38
Elisha Chauhan <elisha.chauhan@spsmail.cuny.edu>
2016-02-08 03:47:43 +0000

    Fixed code using tests

--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Contains globals for new_file and a simple function."""
+
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
