351371a85a7a59d62664e58302d5c107163a26f0
Elisha Chauhan <elisha.chauhan@spsmail.cuny.edu>
2016-02-29 22:25:10 +0000

    Changed through testing

--- task_03.py
+++ task_03.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """Contains globals for new_file and a simple function."""
 
-CHARLIE = "Chocolate Factory"
+CHARLIE = "Brown"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
